b6b99f866a9e998f2b3a3b52f6d68ee6e137b684
Uwakmfon-Ibanga <ibangauwakmfon200@gmail.com>
2024-11-25 10:22:16 +0100

    centred the main content on the home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import styles from './Header.module.css'
 
 const Header = () => {
   return (
-    <div className=" absolute">
-       <img className='pl-5 w-1/2' src="Logo.png" alt="Netisens logo" />
+    <div className=" absolute h-[32px] w-full flex items-end bg-transparent">
+       <img className='pl-[38px] w-[120px]' src="Logo.png" alt="Netisens logo" />
     </div>
   )
 }
